627bfaace338d0d4f1f99a5cdb9f2095a73a28b1
Nat Lund <natjlund@gmail.com>
2022-12-29 21:38:06 +0000

    Change zola_output folder to zola_serve_output

--- zolaserve.bat
+++ zolaserve.bat
@@ -1,1 +1,1 @@
-\Users\natjl\zola-v0.16.1-x86_64-pc-windows-msvc\zola serve --open --output-dir \Users\natjl\Desktop\zola_output
+\Users\natjl\zola-v0.16.1-x86_64-pc-windows-msvc\zola serve --open --output-dir \Users\natjl\Desktop\zola_serve_output
